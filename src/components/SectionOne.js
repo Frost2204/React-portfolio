a6c1a5199a94e890837a79c6a31dbb67b80f3b51
Nikunj Ranja <nikunjranjan2204@gmail.com>
2025-01-23 16:09:03 +0530

    cv link chnage 2

--- src/components/SectionOne.js
+++ src/components/SectionOne.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from 'react';
-import './customStyle/sectionone.css';
-import videoSource from './customStyle/bg.mp4'; // Add your video source path
+import React, { useEffect, useRef } from "react";
+import "./customStyle/sectionone.css";
+import videoSource from "./customStyle/bg.mp4"; // Add your video source path
 
 const SectionOne = () => {
   const spanRef = useRef(null);
@@ -53,7 +53,7 @@ const SectionOne = () => {
         <div className="hero-text">
           <h1 className="display-1 pt-32 font-bold for-rotator">Hi!🐱‍👓</h1>
           <h1 className="display-hero for-rotator font-bold">I'm Nikunj</h1>
-          <span id="word" ref={spanRef} style={{ fontSize: '80px' }}></span>
+          <span id="word" ref={spanRef} style={{ fontSize: "80px" }}></span>
           <span className="cursor"></span>|
           <div className="margin-container-hero">
             <span className="developer-text">Developer</span>
@@ -61,7 +61,7 @@ const SectionOne = () => {
           <div className="margin-50px">
             <div className="button-flex mt-20">
               <a
-                href="https://drive.google.com/file/d/1rHL0-t8ILDtuUL2yYYttk6GA5_BO-vB5/view?usp=sharing"
+                href="https://drive.google.com/file/d/1bPo7WGRFGM3zZfZ4-QPbMQQ4Q2SuQuvB/view?usp=sharing"
                 target="_blank"
                 className="button-purp w-button"
                 rel="noreferrer"
@@ -70,7 +70,11 @@ const SectionOne = () => {
                 Download Resume
               </a>
               <div className="horizontal-divider"></div>
-              <a href="#unity" className="button-hero black w-button" aria-label="View Projects">
+              <a
+                href="#unity"
+                className="button-hero black w-button"
+                aria-label="View Projects"
+              >
                 View Projects
               </a>
             </div>
